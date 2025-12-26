4a07e73b3e8c06455fec7799b212b8bd483f739d
Jerry <jerryleerico@hotmail.com>
2025-12-26 04:10:15 -0800

    Add profile generator scripts and config

---
+++ config.ps1
@@ -0,0 +1,26 @@
+$Config = @{
+    Username  = "NetworkEngCode"
+    BirthYear = 1988
+
+    AsciiArt = "ascii-art.txt"
+
+    OutputFiles = @{
+        Dark  = "dark_mode.svg"
+        Light = "light_mode.svg"
+    }
+
+    Profile = [ordered]@{
+        OS                      = "Windows"
+        Host                    = "N/A"
+        Kernel                  = "Network Automation Engineer"
+        IDE                     = "VS Code"
+        "Languages.Programming" = "Python"
+        Hobbies                 = "Networking, Automation"
+    }
+
+    Contact = [ordered]@{
+        GitHub   = "NetworkEngCode"
+        LinkedIn = "jerryrico"
+    }
+}
+
